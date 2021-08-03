8a6562bb805f5c137851e965cd6ab8750185b81d
choyoungkwang <choyoungkwang@nts-corp.com>
2021-08-03 13:55:27 +0900

    test push

--- src/App.js
+++ src/App.js
@@ -1,2 +1,6 @@
-// index.js
-// This is the main entry point of our application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>Hello Notedly! hahahahah empty project</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
